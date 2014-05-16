2f9b66716d865dff716d67bd18fad9ccbabc2b82
Shiyan LU <developer.shiyan.lu@gmail.com>
2014-05-16 17:25:25 +1000

    Heroku DB

--- config/environments.rb
+++ config/environments.rb
@@ -3,7 +3,7 @@
 configure :production, :development do
 	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/panther')
  
-	ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+	ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/panther')
 
 	# ActiveRecord::Base.establish_connection(
 	# 		:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
