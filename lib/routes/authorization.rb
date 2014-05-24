5527147ca591a64e139c3be7778e0b34231c5451
Shiyan LU <developer.shiyan.lu@gmail.com>
2014-05-24 10:55:23 +1000

    change layout

--- lib/routes/authorization.rb
+++ lib/routes/authorization.rb
@@ -23,26 +23,15 @@ module HashTagTrader
         app.get '/auth/github/callback' do
           session[:uid] = env['omniauth.auth']['uid']
           session[:name] = env['omniauth.auth'][:info][:name]
-    #redirect to('/github_callback')
+
           github_email = puts env['omniauth.auth'][:info][:email]
           user = User.find_by(email: github_email)
           if user==nil
             redirect to('/registration')
-            #haml :registration
           else
             redirect to('/dashboard')
-            #haml :dashboard
           end
 
-
-    #check if the user is already registered within #hashtagTrader
-    #if yes, direct to dashboard
-    #if not, direct to register page
-
-
-
-
-      #haml :auth_github_callback
         end
 
 
@@ -53,11 +42,11 @@ module HashTagTrader
         end
 
 	
-	app.get '/logout' do
-	  session[:uid] = nil
-	  session[:name] = nil
-   	  haml :logout
-	end
+      	app.get '/logout' do
+      	  session[:uid] = nil
+      	  session[:name] = nil
+         	  haml :logout
+      	end
       end
     end
   end
